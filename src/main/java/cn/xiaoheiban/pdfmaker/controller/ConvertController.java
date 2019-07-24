1a23937123adde2e9866599955e58b2f545763e7
wenming <wenming@xiaoheiban.cn>
2019-07-24 17:34:19 +0800

    支持自动替换

--- src/main/java/cn/xiaoheiban/pdfmaker/controller/ConvertController.java
+++ src/main/java/cn/xiaoheiban/pdfmaker/controller/ConvertController.java
@@ -1,5 +1,9 @@
 package cn.xiaoheiban.pdfmaker.controller;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,6 +11,9 @@ import cn.xiaoheiban.pdfmaker.util.FileMakerUtil;
 import cn.xiaoheiban.pdfmaker.execption.StorageFileNotFoundException;
 import cn.xiaoheiban.pdfmaker.service.FileSystemStorageService;
 import cn.xiaoheiban.pdfmaker.service.OssFileService;
+import com.aspose.words.Document;
+import com.fasterxml.jackson.core.JsonParser;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -39,6 +46,32 @@ public class ConvertController {
         try {
             fileSystemStorageService.store(file);
             ossFileService.generateAndUpload(file.getOriginalFilename(), filename, redirectUrl);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return filename;
+    }
+
+    @ResponseBody
+    @PostMapping("/replace")
+    public String handleReplace(@RequestParam("file") MultipartFile file, @RequestParam(name = "callback-url", required = false) String redirectUrl, @RequestParam("replacement") String replacement) {
+        String filename;
+        String newfilename;
+        filename = FileMakerUtil.filename(8) + ".docx";
+        newfilename = FileMakerUtil.filename(8) + ".docx";
+        List<String> values = new LinkedList<String>();
+        JSONObject json = new JSONObject(replacement);
+        Iterator<String> fieldIt = json.keys();
+        List<String> fieldNames = new LinkedList<>();
+        fieldIt.forEachRemaining(obj -> {
+            values.add(json.getString(obj));
+            fieldNames.add(obj);
+        });
+        try {
+            fileSystemStorageService.store(file);
+            Document doc = new Document("/tmp/pdfmake/upload-dir/" + file.getOriginalFilename());
+            doc.getMailMerge().execute(fieldNames.toArray(new String[0]), values.toArray());
+            doc.save("/tmp/pdfmake/generate-dir/" + newfilename);
         } catch (Exception e) {
             System.out.println(e);
         }
